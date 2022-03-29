1a3cf8602cf381580a574749dcfea8d5452b1153
Ricky Jay <58793270+rj-castaneda@users.noreply.github.com>
2022-03-29 10:37:14 -0700

    updated POST to get document id from event id. removed example helloWorld function

--- functions/index.js
+++ functions/index.js
@@ -28,16 +28,8 @@ app.get("/:id", async (req, res) => {
 
 app.post("/", async (req, res) => {
   const room = req.body; // Grabs the Webex Room Details
-  await admin.firestore().collection("webexRoomIds").doc(req.body.title).set(room);
+  await admin.firestore().collection("webexRoomIds").doc(req.body.eventId).set(room);
   res.status(201).send();
 });
 
 exports.webexRoomIds = functions.https.onRequest(app);
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/wrete-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
